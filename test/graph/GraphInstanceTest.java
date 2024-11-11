0cbcf519cc186cddd2836601781dbc82f3c21fb2
faizan-exe <faizanofficial120@gmail.com>
2024-11-11 10:58:13 +0500

    problem set1: done

--- test/graph/GraphInstanceTest.java
+++ test/graph/GraphInstanceTest.java
@@ -19,8 +19,40 @@ import org.junit.Test;
  */
 public abstract class GraphInstanceTest {
     
-    // Testing strategy
-    //   TODO
+    // Testing strategy for each operation of Graph
+    // 
+	// only use of emptyInstance()
+	// Partitions
+	// 
+	// add(L vertex):
+	// 		graph already contains vertex, expect boolean false
+	//		graph does not contain vertex, expect boolean true
+	//
+	// set(L source, L target, int weight):
+	//		directed edge already existed (vertices produced by add method) + non zero weight, expect updated weight
+	//		directed edge did not exist before + non zero weight, create new edge
+	//		directed edge already existed + zero weight, expect removed edge
+	//		directed edge did not exist before + zero weight, expect no mutation
+	//		vertices haven't been added to graph (vertex produced by set method), expect 1, 2 vertices to be added
+	//
+	// remove(L vertex):
+	//		graph contains vertex, expect no edges from or to removed edge, expect boolean true
+	//		graph does not contain vertex, expect false and no mutation
+	//
+	// vertices():
+	//		graph contains 0, 1, >1 vertices
+	//
+	// sources(L target):
+	//		graph contains 0, 1, >1 source vertices
+	//		vertex = produced by add(), produced by set()
+	//
+	// targets(L source):
+	//		graph contains 0, 1, >1 target vertices
+	//		vertex = produced by add(), produced by set()
+	
+	private static final String vertex1 = "v1";
+	private static final String vertex2 = "v2";
+	private static final String vertex3 = "v3";
     
     /**
      * Overridden by implementation-specific test classes.
@@ -41,6 +73,177 @@ public abstract class GraphInstanceTest {
                 Collections.emptySet(), emptyInstance().vertices());
     }
     
-    // TODO other tests for instance methods of Graph
+    @Test
+    public void testAddVertexAlreadyContainsVertex() {
+    	Graph<String> graph = emptyInstance();
+    	graph.add(vertex1);
+    	assertFalse("Expected no modification to the graph since vertex already existed", graph.add(vertex1));
+    }
+    
+    @Test
+    public void testAddVertexDoesntContainVertex() {
+    	Graph<String> graph = emptyInstance();
+    	assertTrue("Expected vertex to be added to graph", graph.add(vertex1));
+    }
+    
+    @Test
+    public void testSetDirectedEdgeAlreadyExistsAndNonZeroWeight() {
+    	Graph<String> graph = emptyInstance();
+    	graph.add(vertex1);
+    	graph.add(vertex2);
+    	graph.set(vertex2, vertex1, 100);
+    	int returnValue = graph.set(vertex2, vertex1, 50);
+    	assertTrue("Expected weighted edge to be updated from 100 to 50", graph.sources(vertex1).containsValue(50));
+    	assertEquals("Expected graph.set(vertex2, vertex1, 50) to return previous edge weight of 100", 100, returnValue);
+    }
+    
+    @Test
+    public void testSetDirectedEdgeDoesntExistAndNonZeroWeight() {
+    	Graph<String> graph = emptyInstance();
+    	graph.add(vertex1);
+    	graph.add(vertex2);
+    	int returnValue = graph.set(vertex2, vertex1, 100);
+    	assertTrue("Expected new weighted edge with value 100", graph.sources(vertex1).containsValue(100));
+    	assertEquals("Expected graph.set(vertex2, vertex1, 100) to return 0, since there was no such edge", 0, returnValue);
+    }
+    
+    @Test
+    public void testSetDirectedEdgeAlreadyExistsAndZeroWeight() {
+    	Graph<String> graph = emptyInstance();
+    	graph.add(vertex1);
+    	graph.add(vertex2);
+    	graph.set(vertex2, vertex1, 100);
+    	int returnValue = graph.set(vertex2, vertex1, 0);
+    	assertFalse("Expected weighted edge to be deleted from sources map", graph.sources(vertex2).containsValue(100));
+    	assertFalse("Expected key vertex1 to be removed from sources map", graph.sources(vertex2).containsKey(vertex1));
+    	assertEquals("Expected graph.set(vertex2, vertex1, 0) to return previous edge weight of 100", 100, returnValue);
+    }
+    
+    @Test
+    public void testSetDirectedEdgeDoesntExistAndZeroWeight() {
+    	Graph<String> graph = emptyInstance();
+    	graph.add(vertex2);
+    	int returnValue = graph.set(vertex2, vertex1, 0);
+    	assertTrue("Expected vertex1 to be added by method set(vertex2, vertex1, 0)", graph.vertices().contains(vertex1));
+    	assertTrue("Expected sources map not to contain keys", graph.sources(vertex2).keySet().isEmpty());
+    	assertEquals("Expected graph.set(vertex2, vertex1, 0) to return 0, since there was no such edge", 0, returnValue);
+    }
+    
+    @Test
+    public void testSetDirectedEdgeOneVertexHasntBeenAdded() {
+    	Graph<String> graph = emptyInstance();
+    	graph.add(vertex1);
+    	int returnValue = graph.set(vertex2, vertex1, 40);
+    	assertTrue("Expected vertex2 to be added by method set(vertex2, vertex1, 40)", graph.sources(vertex1).containsValue(40));
+    	assertEquals("Expected graph.set(vertex2, vertex1, 40) to return 0, sinc there was no such edge", 0, returnValue);
+    }
+    
+    @Test
+    public void testSetDirectedEdgeBothVerticesHaventBeenAdded() {
+    	Graph<String> graph = emptyInstance();
+    	int returnValue = graph.set(vertex2, vertex1, 20);
+    	assertTrue("Expected vertices to be added by method set(vertex2, vertex1, 20)", graph.sources(vertex1).containsValue(20));
+    	assertEquals("Expected graph.set(vertex2, vertex1, 20) to return 0, since there was no such edge", 0, returnValue);
+    }
+    
+    @Test
+    public void testRemoveContainsVertex() {
+    	Graph<String> graph = emptyInstance();
+    	graph.add(vertex1);
+    	graph.add(vertex2);
+    	graph.set(vertex2, vertex1, 10);
+    	graph.remove(vertex2);
+    	assertFalse("Expected weighted edge to be deleted from sources map", graph.sources(vertex2).containsValue(10));
+    	assertFalse("Expected vertex2 to be removed from vertices", graph.vertices().contains(vertex2));
+    }
+    
+    @Test
+    public void testRemoveDoesntContainVertex() {
+    	Graph<String> graph = emptyInstance();
+    	graph.remove(vertex2);
+    	assertTrue("Expected vertices to be empty", graph.vertices().isEmpty());
+    }
+    
+    @Test
+    public void testVerticesContainsNoVertices() {
+    	Graph<String> graph = emptyInstance();
+    	assertTrue("Expected vertices to be empty", graph.vertices().isEmpty());
+    }
+    
+    @Test
+    public void testVerticesContainsOneVertex() {
+    	Graph<String> graph = emptyInstance();
+    	graph.add(vertex2);
+    	assertTrue("Expected vertices to contain vertex2", graph.vertices().contains(vertex2));
+    	assertFalse("Expected vertices not to contain other vertices than vertex2", graph.vertices().contains(vertex1));
+    }
+    
+    @Test
+    public void testVerticesContainsTwoVertices() {
+    	Graph<String> graph = emptyInstance();
+    	graph.add(vertex2);
+    	graph.add(vertex3);
+    	assertTrue("Expected vertices to contain vertex2", graph.vertices().contains(vertex2));
+    	assertTrue("Expected vertices to contain vertex3", graph.vertices().contains(vertex3));
+    	assertFalse("Expected vertices not to contain other vertices than vertex2", graph.vertices().contains(vertex1));
+    }
+    
+    @Test
+    public void testSourcesContainsNoSourceVertices() {
+    	Graph<String> graph = emptyInstance();
+    	assertTrue("Expected sources map not to contain keys", graph.sources(vertex2).keySet().isEmpty());
+    }
+    
+    @Test
+    public void testSourcesContainsOneSourceVertex() {
+    	Graph<String> graph = emptyInstance();
+    	graph.add(vertex1);
+    	graph.add(vertex2);
+    	graph.set(vertex1, vertex2, 200);
+    	assertTrue("Expected sources map to contain key vertex1", graph.sources(vertex2).keySet().contains(vertex1));
+    	assertTrue("Expected sources map to contains value 200", graph.sources(vertex2).containsValue(200));
+    }
+    
+    @Test
+    public void testSourcesContainsTwoSourceVertices() {
+    	Graph<String> graph = emptyInstance();
+    	//vetices 1, 2 and 3 produced by set method
+    	graph.set(vertex3, vertex1, 300);
+    	graph.set(vertex2, vertex1, 150);
+    	assertTrue("Expected sources map to contain key vertex2", graph.sources(vertex1).keySet().contains(vertex2));
+    	assertTrue("Expected sources map to contain key vertex3", graph.sources(vertex1).keySet().contains(vertex3));
+    	assertTrue("Expected sources map to contains value 300", graph.sources(vertex1).containsValue(300));
+    	assertTrue("Expected sources map to contains value 150", graph.sources(vertex1).containsValue(150));
+    }
+    
+    @Test
+    public void testTargetsContainsNoTargetVertices() {
+    	Graph<String> graph = emptyInstance();
+    	graph.add(vertex1);
+    	assertTrue("Expected targets map not to contain keys", graph.targets(vertex1).keySet().isEmpty());
+    }
+    
+    @Test
+    public void testTargetsContainsOneTargetVertex() {
+    	Graph<String> graph = emptyInstance();
+    	//vertices 1 and 3 produced by set method
+    	graph.set(vertex3, vertex1, 250);
+    	assertTrue("Expected targets map to contain key vertex1", graph.targets(vertex3).keySet().contains(vertex1));
+    	assertTrue("Expected targets map to contains value 250", graph.targets(vertex3).containsValue(250));
+    }
+    
+    @Test
+    public void testTargetsContainsTwoTargetVertices() {
+    	Graph<String> graph = emptyInstance();
+    	graph.add(vertex1);
+    	graph.add(vertex2);
+    	graph.add(vertex3);
+    	graph.set(vertex3, vertex1, 500);
+    	graph.set(vertex3, vertex2, 350);
+    	assertTrue("Expected sources map to contain key vertex2", graph.targets(vertex3).keySet().contains(vertex1));
+    	assertTrue("Expected sources map to contain key vertex3", graph.targets(vertex3).keySet().contains(vertex2));
+    	assertTrue("Expected sources map to contains value 500", graph.targets(vertex3).containsValue(500));
+    	assertTrue("Expected sources map to contains value 350", graph.targets(vertex3).containsValue(350));
+    }
     
 }
